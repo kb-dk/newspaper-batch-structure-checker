475ecfe1b54e37d742fb051c95251133e6ed4ba8
Mikis Seth Sørensen <mss@statsbiblioteket.dk>
2013-10-15 17:27:01 +0200

    ChecksumExistenceChecker test ready for review.

--- src/test/java/dk/statsbiblioteket/newspaper/eventhandlers/ChecksumExistenceCheckerTest.java
+++ src/test/java/dk/statsbiblioteket/newspaper/eventhandlers/ChecksumExistenceCheckerTest.java
@@ -4,20 +4,37 @@ import dk.statsbiblioteket.autonomous.ResultCollector;
 import dk.statsbiblioteket.doms.iterator.common.AttributeParsingEvent;
 import org.testng.annotations.Test;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.stub;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.*;
 
 public class ChecksumExistenceCheckerTest {
     @Test
     public void testValidCheckCheck() throws Exception {
-        // Setup fixture
+        // Fixture setup
         ResultCollector resultCollector = mock(ResultCollector.class);
         AttributeParsingEvent validChecksumEvent = mock(AttributeParsingEvent.class);
         stub(validChecksumEvent.getChecksum()).toReturn("aa");
 
+        //Fire stimuli
         ChecksumExistenceChecker checksumExistenceChecker = new ChecksumExistenceChecker(resultCollector);
         checksumExistenceChecker.handleAttribute(validChecksumEvent);
+
+        //Validate
+        verifyNoMoreInteractions(resultCollector);
+    }
+
+    @Test
+    public void testNullCheckTest() throws Exception {
+        // Fixture setup
+        ResultCollector resultCollector = mock(ResultCollector.class);
+        AttributeParsingEvent validChecksumEvent = mock(AttributeParsingEvent.class);
+        stub(validChecksumEvent.getChecksum()).toReturn(null);
+
+        //Fire stimuli
+        ChecksumExistenceChecker checksumExistenceChecker = new ChecksumExistenceChecker(resultCollector);
+        checksumExistenceChecker.handleAttribute(validChecksumEvent);
+
+        //Validate
+        verify(resultCollector).addFailure(anyString(), anyString(), anyString(), anyString());
         verifyNoMoreInteractions(resultCollector);
     }
 }
