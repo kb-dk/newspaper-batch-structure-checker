8eb43681700cd7b58e594bdd5674b8b0bde91d62
jrgatsb <jrg@statsbiblioteket.dk>
2013-10-15 16:53:02 +0200

    Test conforming to recent re-factorings

--- src/test/java/dk/statsbiblioteket/newspaper/BatchStructureCheckerTest.java
+++ src/test/java/dk/statsbiblioteket/newspaper/BatchStructureCheckerTest.java
@@ -1,8 +1,16 @@
 package dk.statsbiblioteket.newspaper;
 
 import dk.statsbiblioteket.autonomous.ResultCollector;
+import dk.statsbiblioteket.doms.iterator.common.NodeBeginsParsingEvent;
+import dk.statsbiblioteket.doms.iterator.common.ParsingEvent;
+import dk.statsbiblioteket.doms.iterator.common.TreeIterator;
+import dk.statsbiblioteket.doms.iterator.filesystem.transforming.TransformingIteratorForFileSystems;
 import dk.statsbiblioteket.newspaper.processmonitor.datasources.Batch;
 import org.junit.Test;
+
+import java.io.File;
+import java.net.URISyntaxException;
+
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -15,14 +23,19 @@ public class BatchStructureCheckerTest {
      */
     @Test
     public void testBatchStructureCheck() throws Exception {
-        BatchStructureChecker batchStructureChecker = new BatchStructureChecker();
+        TreeIterator iterator = getIterator("batch");
+        BatchStructureChecker batchStructureChecker = new BatchStructureChecker(iterator);
         ResultCollector resultCollector = new ResultCollector("Batch Structure Checker", "v0.1");
 
-        batchStructureChecker.checkBatchStructure("batch", resultCollector);
+        batchStructureChecker.checkBatchStructure(resultCollector);
 
         System.out.println("isSuccess: " + resultCollector.isSuccess());
         assertTrue(true);
     }
 
-
+    public TreeIterator getIterator(String batch) throws URISyntaxException {
+        File file = new File(Thread.currentThread().getContextClassLoader().getResource(batch).toURI());
+        System.out.println(file);
+        return new TransformingIteratorForFileSystems(file, "\\.", "\\.jp2$", ".md5");
+    }
 }
