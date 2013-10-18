0d73424be0f5d61a22f81b35d17a21dcf0ff1be2
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2013-10-18 13:48:41 +0200

    Add executable and packaging from sample component

---
+++ src/main/java/dk/statsbiblioteket/newspaper/BatchStructureCheckerExecutable.java
@@ -0,0 +1,45 @@
+package dk.statsbiblioteket.newspaper;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dk.statsbiblioteket.medieplatform.autonomous.AutonomousComponentUtils;
+import dk.statsbiblioteket.medieplatform.autonomous.RunnableComponent;
+
+import java.util.Map;
+import java.util.Properties;
+
+/** This is a sample component to serve as a guide to developers */
+public class BatchStructureCheckerExecutable {
+
+
+    private static Logger log = LoggerFactory.getLogger(BatchStructureCheckerExecutable.class);
+
+    /**
+     * The class must have a main method, so it can be started as a command line tool
+     *
+     * @param args the arguments.
+     *
+     * @throws Exception
+     * @see AutonomousComponentUtils#parseArgs(String[])
+     */
+    public static void main(String[] args)
+            throws
+            Exception {
+        log.info("Starting with args {}", args);
+
+        //Parse the args to a properties construct
+        Properties properties = AutonomousComponentUtils.parseArgs(args);
+
+        //make a new runnable component from the properties
+        RunnableComponent component = new BatchStructureCheckerComponent(properties);
+
+        Map<String, Boolean> result = AutonomousComponentUtils.startAutonomousComponent(properties, component);
+
+        AutonomousComponentUtils.printResults(result);
+
+
+    }
+
+
+}
