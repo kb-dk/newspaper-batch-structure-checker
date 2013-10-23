f6c5b5d8bd123a6448e66064fad95b114aa543de
ktc <ktc@statsbiblioteket.dk>
2013-10-23 15:43:03 +0200

    update edition page node checker

--- src/main/java/dk/statsbiblioteket/newspaper/eventhandlers/EditionPageNodeChecker.java
+++ src/main/java/dk/statsbiblioteket/newspaper/eventhandlers/EditionPageNodeChecker.java
@@ -17,13 +17,19 @@ public class EditionPageNodeChecker extends AbstractNodeChecker {
     private static final String MIX_ATTRIBUTE_SUFFIX = ".mix.xml";
     private static final String MODS_ATTRIBUTE_SUFFIX = ".mods.xml";
     private static final String ALTO_ATTRIBUTE_SUFFIX = ".alto.xml";
+    private static final String JP2_CHILD_NODE_SUFFIX = ".jp2";
     
     private String filePrefix;
     private final String newspaperID;
     private final ResultCollector resultCollector;
     private final TreeNodeState treeNodeState;
     
-    
+    /**
+     * Checker class for edition pages. Checks that:
+     * - All names is of the form [newspaperid]-[parent-node-name]-[pictureID].[suffix]
+     * - There only is the expected attributes {mix, mods, alto}
+     * - Contain the expected child node (child node for the jp2 file). 
+     */
     public EditionPageNodeChecker(String newspaperID, ResultCollector resultCollector, TreeNodeState state) {
         this.newspaperID = newspaperID;
         this.resultCollector = resultCollector;
@@ -52,20 +58,19 @@ public class EditionPageNodeChecker extends AbstractNodeChecker {
     }
     
     private void buildAndCheckFilePrefix() {
-        // prefix is: [newspaperid]-[parent-node-name]-[pictureID]
-        String[] namesplit = name.split("/"); 
-        filePrefix = namesplit[namesplit.length - 1];
-        String parentNodeName = treeNodeState.getCurrentNode().getParent().getName();
-        
-        String currentNodeName = treeNodeState.getCurrentNode().getName();
+        filePrefix = getLocalName(name);
+       
+        String parentName = getLocalName(treeNodeState.getCurrentNode().getName());
         
         String[] nameParts = filePrefix.split("-");
         if(!nameParts[0].equals(newspaperID)) {
-            //report re
+            reportFailure("badnewspaperid", "The name of the page: '" + name + "' does not contain the correct newspaperID ('"
+                    + newspaperID +"')");
         }
         
-        if(!nameParts[1].equals("grandparantlocalname")) {
-            // report bad date and edition
+        if(!nameParts[1].equals(parentName)) {
+            reportFailure("badeditionid", "The name of the page: '" + name + "' does not contain the correct editionID ('"
+                    + parentName +"')");
         }
         
         checkPictureID(nameParts[2]);
@@ -83,6 +88,12 @@ public class EditionPageNodeChecker extends AbstractNodeChecker {
     }
     
     private void checkChildNodes() {
+        if(childNodes.contains(filePrefix + JP2_CHILD_NODE_SUFFIX)) {
+            childNodes.remove(filePrefix + JP2_CHILD_NODE_SUFFIX);
+        }
+        for(String childNode : childNodes) {
+            reportFailure("unexpectedchildnode", "The child node: '" + childNode + "' was not expected.");
+        }
         
     }
     
@@ -106,17 +117,21 @@ public class EditionPageNodeChecker extends AbstractNodeChecker {
                 //A, B, C or D page
                 pageNumber = Integer.parseInt(pictureID.substring(0, 3));
                 if(!pictureID.substring(4).matches("[A-Z]")) {
-                    //report the last letter is not a upper case latin letter.
+                    reportFailure("badpictureid", "The pictureID: '" + pictureID + "' is not of the correct form ([0-9]{4}[A-Z]{0-1})" );
                 }
                 break;
             default:
-                // report bad picture id
+                reportFailure("badpictureid", "The pictureID: '" + pictureID +"' is not of the correct form ([0-9]{4}[A-Z]{0-1})" );
             }
         } catch (NumberFormatException e) {
-            // report pictureID is not a number. 
+            reportFailure("badpictureid", "The pictureID '" + pictureID + "' can't be interpreded as a number."); 
         }
         log.debug("Found page number to be: " + pageNumber);
     }
     
+    private String getLocalName(String fullPath) {
+        String[] namesplit = fullPath.split("/"); 
+        return namesplit[namesplit.length - 1];
+    }
 
 }
