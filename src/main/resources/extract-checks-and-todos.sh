80ef0409d1976f2c6148434a9e753dd802e43315
jrgatsb <jrg@statsbiblioteket.dk>
2013-11-08 15:17:20 +0100

    Documentation of check lister

--- src/main/resources/extract-checks-and-todos.sh
+++ src/main/resources/extract-checks-and-todos.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# The purpose of this script is simply to generate a quick list of "checks" (and TODOs) from
+# demands.sch, the checks being all the different things that demands.sch asserts or reports about
+# the input batch structure in case of an error.
 
 # Extract checks
 grep 'Check:' demands.sch | sed -r 's/^(\ |\t)*<!--\ ?//g' | sed -r 's/\ *(Check)/\1/g' | sed -r 's/\ *-->//g'
