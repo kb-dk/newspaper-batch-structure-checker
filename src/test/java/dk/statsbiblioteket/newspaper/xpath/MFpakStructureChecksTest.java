244af4e078ea96a4c632d9ebeb2e282acaa85302
jrgatsb <jrg@statsbiblioteket.dk>
2013-11-26 13:07:35 +0100

    Cleanup

--- src/test/java/dk/statsbiblioteket/newspaper/xpath/MFpakStructureChecksTest.java
+++ src/test/java/dk/statsbiblioteket/newspaper/xpath/MFpakStructureChecksTest.java
@@ -12,7 +12,6 @@ import java.util.Properties;
 
 public class MFpakStructureChecksTest {
 
-
     @Test(groups = "integrationTest")
     public void testValidate() throws Exception {
         String pathToProperties = System.getProperty("integration.test.newspaper.properties");
@@ -21,19 +20,14 @@ public class MFpakStructureChecksTest {
         properties.load(new FileInputStream(pathToProperties));
         properties.setProperty("scratch", pathToTestBatch + "/" + "small-test-batch");
 
-
         MfPakConfiguration mfPakConfiguration = new MfPakConfiguration();
                 mfPakConfiguration.setDatabaseUrl(properties.getProperty(ConfigurationProperties.DATABASE_URL));
                 mfPakConfiguration.setDatabaseUser(properties.getProperty(ConfigurationProperties.DATABASE_USER));
                 mfPakConfiguration.setDatabasePassword(properties.getProperty(ConfigurationProperties.DATABASE_PASSWORD));
 
-
-
         MFpakStructureChecks mFpakStructureChecks = new MFpakStructureChecks(new MfPakDAO(mfPakConfiguration));
-
 
         ResultCollector resultCollector = new ResultCollector("tool", "version");
         mFpakStructureChecks.validate(new Batch("400022028241"),Thread.currentThread().getContextClassLoader().getResourceAsStream("assumed-valid-structure.xml"),resultCollector);
-
     }
 }
