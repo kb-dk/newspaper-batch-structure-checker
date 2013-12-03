faa115275bf61547da07f4c3affa7e792a181225
jrgatsb <jrg@statsbiblioteket.dk>
2013-12-03 16:56:29 +0100

    Cleanup in test, preparing for move of alto/structure-checks to MFpakStructureChecks

--- src/test/java/dk/statsbiblioteket/newspaper/schematron/StructureValidatorTest.java
+++ src/test/java/dk/statsbiblioteket/newspaper/schematron/StructureValidatorTest.java
@@ -1,13 +1,21 @@
 package dk.statsbiblioteket.newspaper.schematron;
 
 import dk.statsbiblioteket.medieplatform.autonomous.Batch;
+import dk.statsbiblioteket.medieplatform.autonomous.ConfigConstants;
 import dk.statsbiblioteket.medieplatform.autonomous.ResultCollector;
 import dk.statsbiblioteket.medieplatform.autonomous.iterator.common.TreeIterator;
 import dk.statsbiblioteket.medieplatform.autonomous.iterator.eventhandlers.EventRunner;
 import dk.statsbiblioteket.medieplatform.autonomous.iterator.eventhandlers.TreeEventHandler;
 import dk.statsbiblioteket.medieplatform.autonomous.iterator.filesystem.transforming.TransformingIteratorForFileSystems;
 import dk.statsbiblioteket.newspaper.eventhandlers.Util;
+import dk.statsbiblioteket.newspaper.mfpakintegration.configuration.MfPakConfiguration;
+import dk.statsbiblioteket.newspaper.mfpakintegration.database.MfPakDAO;
+import dk.statsbiblioteket.newspaper.mfpakintegration.database.NewspaperBatchOptions;
+import dk.statsbiblioteket.util.xml.DOM;
+import dk.statsbiblioteket.util.xml.XPathSelector;
 import org.testng.annotations.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -17,6 +25,9 @@ import java.util.List;
 import java.util.Properties;
 import java.util.regex.Pattern;
 
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
@@ -26,7 +37,7 @@ import static org.testng.Assert.assertTrue;
 public class StructureValidatorTest {
 
     /**
-     * Checks that running on a well-strcutured batch produces no errors.
+     * Checks that running on a well-structured batch produces no errors.
      * @throws Exception
      */
     @Test(groups = "integrationTest")
@@ -52,11 +63,17 @@ public class StructureValidatorTest {
         String xml = xmlBuilderEventHandler.getXml();
         //System.out.println(xml);
 
-        StructureValidator validator = new StructureValidator("demands.sch");
+        MfPakConfiguration mfPakConfiguration = new MfPakConfiguration();
+        mfPakConfiguration.setDatabaseUrl(properties.getProperty(ConfigConstants.MFPAK_URL));
+        mfPakConfiguration.setDatabaseUser(properties.getProperty(ConfigConstants.MFPAK_USER));
+        mfPakConfiguration.setDatabasePassword(properties.getProperty(ConfigConstants.MFPAK_PASSWORD));
+
+        StructureValidator validator = new StructureValidator("demands.sch", new MfPakDAO(mfPakConfiguration));
         Batch batch = new Batch();
         batch.setRoundTripNumber(1);
         batch.setBatchID("400022028241");
         validator.validate(batch, new ByteArrayInputStream(xml.getBytes("UTF-8")), resultCollector);
+        System.out.println(resultCollector.toReport());
     }
 
 
@@ -64,7 +81,7 @@ public class StructureValidatorTest {
      * Checks that running on a badly-structured batch produces many errors.
      * @throws Exception
      */
-    @Test(groups = "integrationTest")
+    @Test(groups = "integrationTest", enabled = false)
     public void testValidateBadBadBatch() throws Exception {
         String pathToProperties = System.getProperty("integration.test.newspaper.properties");
         String pathToTestBatch = System.getProperty("integration.test.newspaper.testdata");
@@ -84,7 +101,13 @@ public class StructureValidatorTest {
         handlers.add(xmlBuilderEventHandler);
         eventRunner.runEvents(handlers, resultCollector);
         String xml = xmlBuilderEventHandler.getXml();
-        StructureValidator validator = new StructureValidator("demands.sch");
+
+        MfPakConfiguration mfPakConfiguration = new MfPakConfiguration();
+        mfPakConfiguration.setDatabaseUrl(properties.getProperty(ConfigConstants.MFPAK_URL));
+        mfPakConfiguration.setDatabaseUser(properties.getProperty(ConfigConstants.MFPAK_USER));
+        mfPakConfiguration.setDatabasePassword(properties.getProperty(ConfigConstants.MFPAK_PASSWORD));
+
+        StructureValidator validator = new StructureValidator("demands.sch", new MfPakDAO(mfPakConfiguration));
         Batch batch = new Batch();
         batch.setRoundTripNumber(1);
         batch.setBatchID("400022028241");
@@ -92,4 +115,33 @@ public class StructureValidatorTest {
         assertFalse(resultCollector.isSuccess());
         assertTrue(Util.countFailures(resultCollector) >= 40);
     }
+
+
+    /* This commented out code can be deleted when alto-checks have been successfully coded in MFpakStructureChecks
+    @Test
+    public void testValidateCheckAlto() throws Exception {
+        Batch batch = new Batch("400022028241");
+
+        String schematronPath = "demands.sch";
+        MfPakDAO mfPakDAO = mock(MfPakDAO.class);
+        NewspaperBatchOptions options = new NewspaperBatchOptions();
+        options.setOptionB1(true);
+
+        when(mfPakDAO.getBatchOptions(eq(batch.getBatchID()))).thenReturn(options);
+
+        StructureValidator structureValidator = new StructureValidator(schematronPath, mfPakDAO);
+
+        Document document = mock(Document.class);
+        ResultCollector resultCollector = mock(ResultCollector.class);
+        document = structureValidator.setAltoCheckFlagIfNeeded(document, batch, resultCollector);
+
+        String xpathForAlto = "/s:schema/s:let[@name='altoFlag']";
+        final XPathSelector SCHEMATRON_XPATH_SELECTOR = DOM.createXPathSelector("s", "http://purl.oclc.org/dsdl/schematron");
+
+        String flag = ((Element)(SCHEMATRON_XPATH_SELECTOR.selectNode(document, xpathForAlto))).getAttribute("altoFlag");
+
+        assertTrue(flag.equals("true()"));
+    }
+    */
+
 }
