f23c32309faefd8ba4fc9cb4a27f4807e90ca7b0
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2015-08-07 15:01:03 +0200

    Added utility method to get the current method name, in order to print where you are in tests

--- newspaper-batch-structure-checker-common/src/main/java/dk/statsbiblioteket/newspaper/eventhandlers/Util.java
+++ newspaper-batch-structure-checker-common/src/main/java/dk/statsbiblioteket/newspaper/eventhandlers/Util.java
@@ -52,4 +52,16 @@ public class Util {
         String resultCollectorXml = resultCollector.toReport();
         return StringUtils.countMatches(resultCollectorXml, "<failure>");
     }
+
+    /**
+     * Get the method name for a depth in call stack. <br />
+     * Utility function
+     * @param depth depth in the call stack (0 means current method, 1 means call method, ...)
+     * @return method name
+     */
+    public static String getMethodName(final int depth)
+    {
+        final StackTraceElement[] ste = Thread.currentThread().getStackTrace();
+        return ste[2+depth].toString();
+    }
 }
