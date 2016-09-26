e894174603d2c4fdb17462f2c5f78f9e70fdd38f
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2016-09-26 17:24:30 +0200

    Docker commands docced

--- newspaper-batch-structure-checker-component/dockerCommands.sh
+++ newspaper-batch-structure-checker-component/dockerCommands.sh
@@ -1,6 +1,32 @@
 #!/usr/bin/env bash
+
+#Clean all and rebuild
 docker ps -a | grep checker | cut -d' ' -f1 | xargs docker rm -v; docker rmi -f structure-checker; docker build -t structure-checker .
 
 
+#bypass entrypoint
+docker run -e USER_ID=$(id -u) --name 'structure-checker-test' -v $(pwd)/target:/home/newspapr/logs -it --entrypoint='bash' structure-checker
 
- docker run -e USER_ID=$(id -u) --name 'structure-checker-test' -v $(pwd)/target:/home/newspapr/logs -it --entrypoint='bash' structure-checker
+#Remove dangling containers
+docker rm $(docker ps -a -q)
+
+#Remove dangling images
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+
+
+#Run the autonomous component in a docker
+
+docker run \
+    -e USER_ID=$(id -u) \
+    -v $(pwd)/target/logs:/home/newspapr/logs \
+    -v $(pwd)/src/main/config:/home/newspapr/conf \
+    statsbiblioteket/newspaper-batch-structure-checker-component
+
+#Set the docker users UID to the invoking user. Nessesary for having write permissions to the log dir (if used)
+#   -e USER_ID=$(id -u)
+
+#Specify an external folder to hold the logfiles
+#   -v $(pwd)/target/logs:/home/newspapr/logs
+
+#Specify an external folder with the config files
+#    -v $(pwd)/src/main/config:/home/newspapr/conf \
