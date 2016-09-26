eda353a38dec37c17b66e978220c5670fd68b851
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2016-09-26 17:34:47 +0200

    Only set docker UID if set

--- newspaper-batch-structure-checker-component/src/main/scripts/entry.sh
+++ newspaper-batch-structure-checker-component/src/main/scripts/entry.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-#echo $USER_ID # 1000 From docker run command line
+#echo $USER_ID # From docker run command line
 #echo $user #From dockerfile ENV
 
-usermod -u $USER_ID $user #Change uid to the specified user, so mount points work correctly
+if [[ -v "${USER_ID// }" ]]; then
+    usermod -u $USER_ID $user #Change uid to the specified user, so mount points work correctly
+fi
 
 sudo -u $user java -classpath conf:lib/* dk.statsbiblioteket.newspaper.BatchStructureCheckerExecutable -c conf/config.properties
