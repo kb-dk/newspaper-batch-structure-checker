2541d0558701ee801ebe35cd7eabfab55e9a6cee
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2016-09-27 11:35:04 +0200

    Entry.sh correctly checks for user id and group id

--- newspaper-batch-structure-checker-component/src/main/scripts/entry.sh
+++ newspaper-batch-structure-checker-component/src/main/scripts/entry.sh
@@ -3,8 +3,13 @@
 #echo $USER_ID # From docker run command line
 #echo $user #From dockerfile ENV
 
-if [[ -v "${USER_ID// }" ]]; then
+
+if [[ -n "${USER_ID// }" ]]; then
     usermod -u $USER_ID $user #Change uid to the specified user, so mount points work correctly
+fi
+
+if [[ -n "${GROUP_ID// }" ]]; then
+    usermod -g $GROUP_ID $user #Change gid to the specified user, so mount points work correctly
 fi
 
 sudo -u $user java -classpath conf:lib/* dk.statsbiblioteket.newspaper.BatchStructureCheckerExecutable -c conf/config.properties
